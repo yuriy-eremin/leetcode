f7c807b43beb30c87c76f098c509eec028b391f2
Yuriy Eremin <ereminyuv@gmail.com>
2019-10-06 02:21:33 +0700

    771. Jewels and Stones closes #4

--- src/test/scala/com/leetcode/SolutionSpec.scala
+++ src/test/scala/com/leetcode/SolutionSpec.scala
@@ -1,10 +1,17 @@
 package com.leetcode
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{Matchers, WordSpec}
 
-class SolutionSpec extends FlatSpec with Matchers {
-  "Solution" should "defangIPaddr" in {
-    Solution.defangIPaddr("1.1.1.1") shouldEqual "1[.]1[.]1[.]1"
-    Solution.defangIPaddr("255.100.50.0") shouldEqual "255[.]100[.]50[.]0"
+class SolutionSpec extends WordSpec with Matchers {
+  "Solution" should {
+    "defangIPaddr" in {
+      Solution.defangIPaddr("1.1.1.1") shouldEqual "1[.]1[.]1[.]1"
+      Solution.defangIPaddr("255.100.50.0") shouldEqual "255[.]100[.]50[.]0"
+    }
+
+    "numJewelsInStones" in {
+      Solution.numJewelsInStones(J = "aA", S = "aAAbbbb") shouldEqual 3
+      Solution.numJewelsInStones(J = "z", S = "ZZ") shouldEqual 0
+    }
   }
 }
